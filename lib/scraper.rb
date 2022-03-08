a460db9a1b28382194cd2fff5ca3d569b67f596b
Read Langworthy <rlangworthy@gmail.com>
2022-03-08 15:41:54 -0800

    commit tree 1a8a5e4a7d7f1d91202378aed6694c294933f2bc

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
+require "pry"
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+
+doc_css = doc.css(".heading-40-semibold color-black mb-20")
+
+
+
+binding.pry
